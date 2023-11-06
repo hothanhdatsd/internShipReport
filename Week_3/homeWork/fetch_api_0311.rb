4010251cbfe11dff25bd8053cf4e2a9fc8c5d34d
HoThanhDat <hothanhdatsd@gmail.com>
2023-11-06 08:55:10 +0700

    commit

--- Week_3/homeWork/fetch_api_0311.rb
+++ Week_3/homeWork/fetch_api_0311.rb
@@ -5,31 +5,35 @@ require 'faraday'
 conn = Faraday.new(url: 'https://6418014ee038c43f38c45529.mockapi.io/api/v1/users')
 
 # # lay thong tin user
-response = conn.get do |req|
-  req.params['active'] = 'true'
-end
+# response = conn.get do |req|
+#   req.params['active'] = 'true'
+# end
 
-p response
+# p response
 
 # tao user
 
-user = {
-  'name' => 'Thanh Dat',
-  'sex' => 'male',
-  'created_at' => DateTime.now.to_s,
-  'avatar' => 'https://duhocvietglobal.com/wp-content/uploads/2018/12/dat-nuoc-va-con-nguoi-anh-quoc.jpg'
-}
+# user = {
+#   'name' => 'Thanh Dat',
+#   'sex' => 'male',
+#   'created_at' => DateTime.now.to_s,
+#   'avatar' => 'https://duhocvietglobal.com/wp-content/uploads/2018/12/dat-nuoc-va-con-nguoi-anh-quoc.jpg'
+# }
 
-respone = conn.post do |req|
-  req.headers['Content-Type'] = 'application/json'
-  req.body = user.to_json
-end
-p respone
+# respone = conn.post do |req|
+#   req.headers['Content-Type'] = 'application/json'
+#   req.body = user.to_json
+# end
+# p respone
 
 # # xoa user
 
-user_id = 187
-response = conn.delete "/users/#{user_id}"
+user_id = 185
+response = conn.delete do |req|
+  req.url "#{user_id}"
+  req.headers['Content-Type'] = 'application/json'
+
+end
 
 if response.status == 200
   puts "Người dùng với ID #{user_id} đã được xóa thành công."
