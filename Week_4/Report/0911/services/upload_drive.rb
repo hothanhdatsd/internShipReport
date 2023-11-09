afb43b94234d3d54e0e98d3ed4aadd0e367d8938
HoThanhDat <hothanhdatsd@gmail.com>
2023-11-09 15:25:11 +0700

    create feat upload to drive and use faker

--- Week_4/Report/0911/services/upload_drive.rb
+++ Week_4/Report/0911/services/upload_drive.rb
@@ -1,66 +1,93 @@
+# frozen_string_literal: true
+
 require 'google_drive'
 
-session = GoogleDrive::Session.from_service_account_key("key.json")
+# upload file to drive
+class UploadDrive
+  # Xem file tren session
+  def self.list_file
+    session.files.each do |file|
+      p file
+    end
+  end
 
+  # xem file trong folder
+  def self.list_in_folder(name_folder)
+    folder_id = take_id_folder(name_folder)
+    files_in_folder = session.collection_by_id(folder_id).files
+    files_in_folder.each do |file|
+      puts "ID của tệp: #{file.id}"
+      puts "Tên của tệp: #{file.title}"
+      puts "Đường dẫn trực tiếp: #{file.human_url}"
+      puts '---------'
+    end
+  end
 
+  def self.create_folder(name)
+    session.create_collection(name)
+  end
 
-# Tạo một thư mục trên Google Drive
-# session.files.each do |file|
-#   p file
-# end
+  # upload file  to folder
+  def self.upload_to_folder(folder_name, file_name, name_file_on_drive)
+    target_folder = session.collections.select { |collection| collection.title == folder_name }.first
+    if target_folder.nil?
+      target_folder = session.create_collection(folder_name)
+      puts "Thư mục mới đã được tạo: #{target_folder.title}"
+    end
+    # docx_file_path = 'data/TableApi.docx'
+    target_folder.upload_from_file(file_name, name_file_on_drive, convert: true)
+  end
 
-# #xem file trong folder 
-# files_in_folder = session.collection_by_id("1mI0nQSMqBNyBbQk4BxbEX41jynUZGCFJ").files
-# files_in_folder.each do |file|
-#   puts "ID của tệp: #{file.id}"
-#   puts "Tên của tệp: #{file.title}"
-#   puts "Đường dẫn trực tiếp: #{file.human_url}"
-#   puts "---------"
-# end
+  # share folder to user
+  def self.share_folder(name_folder, name_user)
+    folder_id = take_id_folder(name_folder)
+    folder = session.folder_by_id(folder_id)
+    permission = {
+      type: 'user', role: 'writer', email_address: name_user
+    }
+    folder.acl.push(permission)
+    puts "Folder shared with: #{name_user}"
+  end
 
+  # lay id cua folder
+  def self.take_id_folder(str)
+    session.folders do |folder|
+      return folder.id if folder.title == str
+    end
+  end
 
-# tao folder
-# folder_name = "Data"
-# folder = session.create_collection(folder_name)
+  # lay session
+  def self.session
+    GoogleDrive::Session.from_service_account_key('key.json')
+  end
 
-# folder_name_to_search = "Data"
-# target_folder = session.collections.select { |collection| collection.title == folder_name_to_search }.first
-# if target_folder.nil?
-#   target_folder = session.create_collection(folder_name_to_search)
-#   puts "Thư mục mới đã được tạo: #{target_folder.title}"
-# end
-# docx_file_path = "data/TableChart.docx"
-# uploaded_file = target_folder.upload_from_file(docx_file_path, "TableChart.txt", convert: true)
-
+  # list folders
+  def self.list_folders
+    session.folders do |folder|
+      p folder
+    end
+  end
+end
 
 # upload file
-# docx_file_path = "data/TableChart.docx"
-# uploaded_file = session.upload_from_file(docx_file_path, "TableChart.txt", convert: true)
+# docx_file_path = 'data/TableChart.docx'
+# uploaded_file = session.upload_from_file(docx_file_path, 'TableChart.txt', convert: true)
 # if uploaded_file
-#   puts "Tệp đã tải lên thành công!"
+#   puts 'Tệp đã tải lên thành công!'
 #   puts "ID của tệp: #{uploaded_file.id}"
 #   puts "Tên của tệp: #{uploaded_file.title}"
 #   puts "Đường dẫn trực tiếp: #{uploaded_file.human_url}"
 #   p uploaded_file
 # else
-#   puts "Tải lên tệp không thành công."
+#   puts 'Tải lên tệp không thành công.'
 # end
 
-# file = session.file_by_id("1kKygKGeNABE75QUK2WmfFoG3UeleJFzA")
-# p file
-# # Kiểm tra xem tệp có tồn tại không
-# if file
-#   # Tải xuống tệp và lưu vào đường dẫn cụ thể
-#   file.download_to_file("data/FreebieMarket.zip")
-#   # p file.methods
+## download
+# folder = session.folder_by_id('1mI0nQSMqBNyBbQk4BxbEX41jynUZGCFJ')
+# if folder
 #   puts "Tệp đã được tải xuống thành công và lưu trong thư mục 'data'."
 # else
 #   puts "Không tìm thấy tệp có tiêu đề 'TableApi'."
 # end
 
-
-user_email = 'hothanhdatsd@gmail.com'
-folder_id = '1mI0nQSMqBNyBbQk4BxbEX41jynUZGCFJ'
-folder = session.folder_by_id(folder_id)
-folder.acl.push(type: 'user', role: 'writer', email_address: user_email)
-puts "Folder shared with: #{user_email}"
+UploadDrive.share_folder('TestTest', 'hothanhdatsd@gmail.com')
---
+++ Week_4/Report/0911/services/useGemFaker.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'faker'
+require 'faraday'
+# class user
+class User
+  API_URL = 'https://6418014ee038c43f38c45529.mockapi.io/api/v1/users'
+  attr_reader :name, :avatar, :sex
+
+  def initialize(hash)
+    @name = hash['name']
+    @avatar = hash['avatar']
+    @sex = hash['sex']
+    @created_at = DateTime.now.to_s
+  end
+
+  def self.post_data
+    url.post do |req|
+      req.body = {
+        name: Faker::Name.name,
+        avatar: Faker::Avatar.image,
+        sex: Faker::Gender.binary_type,
+        active: Faker::Boolean.boolean
+      }
+    end
+  end
+
+  def self.url
+    @url ||= Faraday.new(url: API_URL)
+  end
+
+  private
+
+  def to_json(*_args)
+    JSON.generate(self)
+  end
+end
+
+User.post_data
